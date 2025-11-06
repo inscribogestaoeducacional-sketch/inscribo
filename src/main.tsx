494b42ecb9e88b46f633ae130474b32e6aa1dfb3
inscribogestaoeducacional-sketch <inscribogestaoeducacional@gmail.com>
2025-11-06 10:55:14 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { setupReloadPrevention, setupSessionPersistence } from './lib/preventReload'
+
+// ADICIONE AQUI ↓
+setupReloadPrevention()
+setupSessionPersistence()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
