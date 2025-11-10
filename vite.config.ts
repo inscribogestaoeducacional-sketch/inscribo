d1b4a202cf6e7bb72127532f96994fdd1b5f1b0e
inscribogestaoeducacional-sketch <inscribogestaoeducacional@gmail.com>
2025-11-10 16:37:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-router-dom']
+  base: '/',
+  server: {
+    port: 3000,
+    host: true,
+    cors: true,
+    hmr: {
+      overlay: false
+    }
   },
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
+        manualChunks: undefined,
+      },
     },
+    chunkSizeWarningLimit: 2000,
+    sourcemap: false,
     commonjsOptions: {
-      include: [/node_modules/]
+      include: [/node_modules/],
+      transformMixedEsModules: true
     }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+    force: true
   }
 })
