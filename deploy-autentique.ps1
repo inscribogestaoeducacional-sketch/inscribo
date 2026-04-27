85869a7bece6734aacad23f117fbba4f33bf51b8
victorprojeto90-alt <victorprojeto90@gmail.com>
2026-04-26 21:16:08 -0300

    gmh

---
+++ deploy-autentique.ps1
@@ -0,0 +1,3 @@
+$env:SUPABASE_ACCESS_TOKEN="sbp_c411d000510e00168bfc8c4291031a40c6295cbe"
+.\supabase.exe functions deploy autentique --no-verify-jwt --project-ref syxxuumxkhhnoqrxporj
+Write-Host "Autentique deployado!"
